0238456e9c5f602491478eb10b3d3b3f23f23df1
kasha <karam.ny@gmail.com>
2017-06-05 20:09:04 -0500

    Longest Bitonic

---
+++ Algorithms/DynamicProgramming/LongestBitonicSubsequence.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+// http://www.geeksforgeeks.org/dynamic-programming-set-15-longest-bitonic-subsequence/
+namespace Algorithms.DynamicProgramming
+{
+    public class LongestBitonicSubsequence
+    {
+        public void getLongestBitonic(int[] seq)
+        {
+            int[] lis = new int[seq.Length];
+            int[] lis_reverse = new int[seq.Length];
+
+            int n = seq.Length - 1;
+            for (int i = 0; i < seq.Length; i++)
+            {
+                lis_reverse[n - i] = 1; lis[i] = 1;
+                for (int j = 0; j < i; j++)
+                {
+                    if (seq[j] < seq[i])
+                        lis[i] = Math.Max(lis[i], lis[j] + 1);
+                    else if (seq[n - j] < seq[n - i])
+                        lis_reverse[n - i] = Math.Max(lis_reverse[n - i], lis_reverse[n - j] + 1);
+                    else
+                    {
+                        lis[i] = lis[j];
+                        lis_reverse[n - i] = lis_reverse[n - j];
+                    }
+                }
+            }
+
+            int res = 0;
+            for (int i = 0; i < n; i++)
+            {
+                // LIS + LIS_Reverse (reverse increasing curve which will be the decreasing curve) - 1(-1 is to eliminate the common
+                // shared point between LIS and LIS_reverse)
+                res = Math.Max(res, lis[i] + lis_reverse[i] - 1);
+            }
+
+            Console.WriteLine(res);
+        }
+    }
+}
--- Algorithms/Source.cs
+++ Algorithms/Source.cs
@@ -13,7 +13,9 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            
+            int[] s = { 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15 };
+            DP.LongestBitonicSubsequence bitonic = new DP.LongestBitonicSubsequence();
+            bitonic.getLongestBitonic(s);
             Console.ReadLine();
         }
 
